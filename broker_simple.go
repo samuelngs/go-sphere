9db56c69d19873fefe16e23dca6e328dca5d2e83
Samuel <sam@infinitely.io>
2015-12-28 02:45:25 -0700

    provide simple broker

---
+++ broker_simple.go
@@ -0,0 +1,33 @@
+package sphere
+
+// NewSimpleBroker creates a new instance of SimpleBroker
+func NewSimpleBroker() *SimpleBroker {
+	return &SimpleBroker{
+		ExtendBroker(),
+	}
+}
+
+// SimpleBroker is a broker adapter built on Simple client
+type SimpleBroker struct {
+	*Broker
+}
+
+// OnSubscribe when websocket subscribes to a channel
+func (broker *SimpleBroker) OnSubscribe(channel *Channel) error {
+	return nil
+}
+
+// OnUnsubscribe when websocket unsubscribes from a channel
+func (broker *SimpleBroker) OnUnsubscribe(channel *Channel) error {
+	return nil
+}
+
+// OnPublish when websocket publishes data to a particular channel from the current broker
+func (broker *SimpleBroker) OnPublish(channel *Channel, data interface{}) error {
+	return nil
+}
+
+// OnMessage when websocket receive data from the broker subscriber
+func (broker *SimpleBroker) OnMessage(channel *Channel, data interface{}) error {
+	return nil
+}
