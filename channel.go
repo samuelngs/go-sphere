d39333b634fed9ee2673e659c892ebdb98c8852c
Samuel <sam@infinitely.io>
2015-12-27 20:03:07 -0700

    add channel name and state props to channel class

--- channel.go
+++ channel.go
@@ -2,4 +2,6 @@ package sphere
 
 // Channel let you subscribe to and watch for incoming data which is published on that channel by other clients or the server
 type Channel struct {
+	name  string
+	state ChannelState
 }
