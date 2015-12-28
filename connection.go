15cc1bd2a083aa0eca2564347e68544f8dc30e5a
Samuel <sam@infinitely.io>
2015-12-27 23:44:46 -0700

    new Sphere Connection class

--- connection.go
+++ connection.go
@@ -1,5 +1,26 @@
 package sphere
 
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// NewConnection returns a new ws connection instance
+func NewConnection(w http.ResponseWriter, r *http.Request) (*Connection, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err == nil {
+		return &Connection{[]*Channel{}, r, conn}, nil
+	}
+	return nil, err
+}
+
 // Connection allows you to interact with backend and other client sockets in realtime
 type Connection struct {
+	// list of channels that this connection has been subscribed
+	channels []*Channel
+	// http header
+	*http.Request
+	// websocket connection
+	*websocket.Conn
 }
