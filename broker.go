f7c0377899f52157207f3829699cd70821336eb2
Samuel <sam@infinitely.io>
2015-12-27 21:02:01 -0700

    rename NewBroker to ExtendBroker, add OnMessage, update Agent interface

--- broker.go
+++ broker.go
@@ -9,42 +9,47 @@ const (
 	BrokerErrorOverrideOnUnsubscribe = "please override OnUnsubscribe"
 	// BrokerErrorOverrideOnPublish to warn use to override OnPublish function
 	BrokerErrorOverrideOnPublish = "please override OnPublish"
+	// BrokerErrorOverrideOnMessage to warn use to override OnMessage function
+	BrokerErrorOverrideOnMessage = "please override OnMessage"
 )
 
 // Agent represents Broker instance
 type Agent interface {
-	OnSubscribe()   // => Broker OnSubscribe
-	OnUnsubscribe() // => Broker OnUnsubscribe
-	OnPublish()     // => Broker OnPublish
+	OnSubscribe(*Channel)            // => Broker OnSubscribe
+	OnUnsubscribe(*Channel)          // => Broker OnUnsubscribe
+	OnPublish(*Channel, interface{}) // => Broker OnPublish
+	OnMessage(*Channel, interface{}) // => Broker OnMessage
 }
 
-// NewBroker creates a broker instance
-func NewBroker() *Broker {
+// ExtendBroker creates a broker instance
+func ExtendBroker() *Broker {
 	return &Broker{
-		id:       "",
 		channels: make(map[string]*Channel),
 	}
 }
 
 // Broker allows you to interact directly with Websocket internal data and pub/sub channels
 type Broker struct {
-	// The broker's id
-	id string
 	// List of channels
 	channels map[string]*Channel
 }
 
 // OnSubscribe when websocket subscribes to a channel
-func (broker *Broker) OnSubscribe() error {
+func (broker *Broker) OnSubscribe(channel *Channel) error {
 	return errors.New(BrokerErrorOverrideOnSubscribe)
 }
 
 // OnUnsubscribe when websocket unsubscribes from a channel
-func (broker *Broker) OnUnsubscribe() error {
+func (broker *Broker) OnUnsubscribe(channel *Channel) error {
 	return errors.New(BrokerErrorOverrideOnUnsubscribe)
 }
 
 // OnPublish when websocket publishes data to a particular channel from the current broker
 func (broker *Broker) OnPublish(channel *Channel, data interface{}) error {
 	return errors.New(BrokerErrorOverrideOnPublish)
+}
+
+// OnMessage when websocket receive data from the broker subscriber
+func (broker *Broker) OnMessage(channel *Channel, data interface{}) error {
+	return errors.New(BrokerErrorOverrideOnMessage)
 }
