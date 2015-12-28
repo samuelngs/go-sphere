9b426d45b2467e65abb4bf45a0b58a3758137f5a
Samuel <sam@infinitely.io>
2015-12-27 23:45:09 -0700

    add Connection array and handler func

--- sphere.go
+++ sphere.go
@@ -1,6 +1,29 @@
 package sphere
 
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
 // Sphere represents an entire Websocket instance
 type Sphere struct {
+	// a broker agent
 	agent *Agent
+	// list of active connections
+	connections []*Connection
+}
+
+// Handler handles and creates websocket connection
+func (sphere *Sphere) Handler(w http.ResponseWriter, r *http.Request) {
+	if _, err := NewConnection(w, r); err == nil {
+	} else {
+	}
 }
