885c73bd4a77754658b56b034f5cf0b668b70ac8
Samuel <sam@infinitely.io>
2015-12-30 19:08:25 -0700

    new channel func

--- channel.go
+++ channel.go
@@ -1,10 +1,17 @@
 package sphere
 
+import "github.com/streamrail/concurrent-map"
+
+// NewChannel creates new Channel instance
+func NewChannel(name string) *Channel {
+	return &Channel{name: name, state: ChannelStatePending, connections: cmap.New()}
+}
+
 // Channel let you subscribe to and watch for incoming data which is published on that channel by other clients or the server
 type Channel struct {
 	name        string
 	state       ChannelState
-	connections map[string]*Connection
+	connections cmap.ConcurrentMap
 }
 
 // Name returns the name of the channel
@@ -20,8 +27,8 @@ func (channel *Channel) State() ChannelState {
 // Connections returns a list of active user connections
 func (channel *Channel) Connections() []*Connection {
 	conns := make([]*Connection, 0, len(channel.connections))
-	for _, conn := range channel.connections {
-		conns = append(conns, conn)
+	for item := range channel.connections.Iter() {
+		conns = append(conns, item.Val.(*Connection))
 	}
 	return conns
 }
