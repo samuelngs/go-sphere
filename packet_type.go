980c0490462c588ff96d95181d1bd0fe007d6e2b
Samuel <sam@infinitely.io>
2015-12-31 00:22:28 -0700

    new packet type with json support

---
+++ packet_type.go
@@ -0,0 +1,69 @@
+package sphere
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+// PacketType indicates the error of the channel
+type PacketType int
+
+const (
+	// PacketTypeMessage denotes a regular text data message.
+	PacketTypeMessage PacketType = iota
+	// PacketTypeChannel denotes a channel request.
+	PacketTypeChannel
+	// PacketTypeSubscribe denotes a subscribe request.
+	PacketTypeSubscribe
+	// PacketTypeUnsubscribe denotes an unsubscribe request.
+	PacketTypeUnsubscribe
+	// PacketTypePing denotes an ping message.
+	PacketTypePing
+	// PacketTypePong denotes an pong message.
+	PacketTypePong
+	// PacketTypeUnknown denotes an pong message.
+	PacketTypeUnknown
+)
+
+// PacketTypeCode returns the string value of SphereError
+var PacketTypeCode = [...]string{
+	"message",
+	"channel",
+	"subscribe",
+	"unsubscribe",
+	"ping",
+	"pong",
+	"unknown",
+}
+
+// Returns the error message
+func (p PacketType) String() string {
+	return PacketTypeCode[p]
+}
+
+// UnmarshalJSON to parse object from json string
+func (p *PacketType) UnmarshalJSON(b []byte) (err error) {
+	m := strings.Trim(string(b[:]), `"`)
+	switch m {
+	case PacketTypeCode[0]:
+		*p = PacketTypeMessage
+	case PacketTypeCode[1]:
+		*p = PacketTypeChannel
+	case PacketTypeCode[2]:
+		*p = PacketTypeSubscribe
+	case PacketTypeCode[3]:
+		*p = PacketTypeUnsubscribe
+	case PacketTypeCode[4]:
+		*p = PacketTypePing
+	case PacketTypeCode[5]:
+		*p = PacketTypePong
+	default:
+		*p = PacketTypeUnknown
+	}
+	return nil
+}
+
+// MarshalJSON to convert PacketType to json string
+func (p *PacketType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.String())
+}
