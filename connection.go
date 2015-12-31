6d8ec953103a84560707319fd05b1085989737f4
Samuel <sam@infinitely.io>
2015-12-31 02:31:40 -0700

    connection improvments

--- connection.go
+++ connection.go
@@ -9,9 +9,11 @@ import (
 
 // NewConnection returns a new ws connection instance
 func NewConnection(w http.ResponseWriter, r *http.Request) (*Connection, error) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err == nil {
-		return &Connection{guid.String(), []*Channel{}, make(chan []byte), r, conn}, nil
+		conn := &Connection{guid.String(), []*Channel{}, make(chan []byte), make(chan *Packet), r, ws}
+		go conn.queue()
+		return conn, nil
 	}
 	return nil, err
 }
@@ -24,50 +26,24 @@ type Connection struct {
 	channels []*Channel
 	// buffered channel of outbound messages
 	send chan []byte
+	// buffered channel of inbound messages
+	receive chan *Packet
 	// http request
 	request *http.Request
 	// websocket connection
 	*websocket.Conn
 }
 
+func (conn *Connection) queue() {
+	for {
+		select {
+		case <-conn.receive:
+		}
+	}
+}
+
 // write writes a message with the given message type and payload.
 func (conn *Connection) emit(mt int, payload []byte) error {
 	conn.SetWriteDeadline(time.Now().Add(writeWait))
 	return conn.WriteMessage(mt, payload)
-}
-
-// writePump pumps messages from the sphere to the websocket connection.
-func (conn *Connection) writePump() error {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-	}()
-	for {
-		select {
-		case msg, ok := <-conn.send:
-			if !ok {
-				return conn.emit(websocket.CloseMessage, []byte{})
-			}
-			if err := conn.emit(websocket.TextMessage, msg); err != nil {
-				return err
-			}
-		case <-ticker.C:
-			if err := conn.emit(websocket.PingMessage, []byte{}); err != nil {
-				return err
-			}
-		}
-	}
-}
-
-func (conn *Connection) readPump() {
-	conn.SetReadLimit(maxMessageSize)
-	conn.SetReadDeadline(time.Now().Add(pongWait))
-	conn.SetPingHandler(func(string) error { conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-		}
-		if msg != nil {
-		}
-	}
 }
