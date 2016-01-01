569d6e276dd9f5ec36ebf9147f6b1a32c8cba9c3
Samuel Ng <sam@infinitely.io>
2015-12-31 17:34:48 -0700

    use core error

--- error.go
+++ error.go
@@ -1,21 +1,14 @@
 package sphere
 
-// Error Error struct
-type Error struct {
-	ErrorString string
-}
-
-// Error returns ErrorString message
-func (err *Error) Error() string {
-	return err.ErrorString
-}
+import "errors"
 
 // List of errors
 var (
-	ErrUnsupportedNamespace = &Error{"unsupported namespace"}
-	ErrNotImplemented       = &Error{"not implemented"}
-	ErrBadScheme            = &Error{"bad scheme"}
-	ErrBadStatus            = &Error{"bad status"}
-	ErrBadRequestMethod     = &Error{"bad method"}
-	ErrNotSupported         = &Error{"not supported"}
+	ErrNotAuthorized        = errors.New("not authorized")
+	ErrUnsupportedNamespace = errors.New("unsupported namespace")
+	ErrNotImplemented       = errors.New("not implemented")
+	ErrBadScheme            = errors.New("bad scheme")
+	ErrBadStatus            = errors.New("bad status")
+	ErrBadRequestMethod     = errors.New("bad method")
+	ErrNotSupported         = errors.New("not supported")
 )
