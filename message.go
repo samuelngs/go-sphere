ff2cecc0c193cc0d45a9e92bd232ed33bc17c7c1
Samuel <sam@infinitely.io>
2016-01-01 02:12:25 -0700

    update message struct

--- message.go
+++ message.go
@@ -2,6 +2,6 @@ package sphere
 
 // Message indicates the data of the message
 type Message struct {
-	Event string      `json:"event,omitempty"`
-	Data  interface{} `json:"data,omitempty"`
+	Event string `json:"event,omitempty"`
+	Data  string `json:"data,omitempty"`
 }
