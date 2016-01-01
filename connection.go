cca6410d1b92cf07c190d927893eed94ff44a071
Samuel <sam@infinitely.io>
2016-01-01 02:12:02 -0700

    add subscribe, unsubscribe, isSubscribed func(s)

--- connection.go
+++ connection.go
@@ -5,13 +5,14 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/streamrail/concurrent-map"
 )
 
 // NewConnection returns a new ws connection instance
 func NewConnection(w http.ResponseWriter, r *http.Request) (*Connection, error) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err == nil {
-		conn := &Connection{guid.String(), 0, []*Channel{}, make(chan []byte), make(chan *Packet), r, ws}
+		conn := &Connection{guid.String(), 0, cmap.New(), make(chan []byte), make(chan *Packet), r, ws}
 		go conn.queue()
 		return conn, nil
 	}
@@ -25,7 +26,7 @@ type Connection struct {
 	// cid
 	cid int
 	// list of channels that this connection has been subscribed
-	channels []*Channel
+	channels cmap.ConcurrentMap
 	// buffered channel of outbound messages
 	send chan []byte
 	// buffered channel of inbound messages
@@ -48,4 +49,31 @@ func (conn *Connection) queue() {
 func (conn *Connection) emit(mt int, payload []byte) error {
 	conn.SetWriteDeadline(time.Now().Add(writeWait))
 	return conn.WriteMessage(mt, payload)
+}
+
+// subscribe to channel
+func (conn *Connection) subscribe(channel *Channel) error {
+	if !conn.isSubscribed(channel) {
+		conn.channels.Set(channel.Name(), channel)
+	}
+	if !channel.connections.Has(conn.id) {
+		conn.channels.Set(conn.id, conn)
+	}
+	return nil
+}
+
+// unsubscribe from channel
+func (conn *Connection) unsubscribe(channel *Channel) error {
+	if conn.isSubscribed(channel) {
+		conn.channels.Remove(channel.Name())
+	}
+	if channel.connections.Has(conn.id) {
+		conn.channels.Remove(conn.id)
+	}
+	return nil
+}
+
+// isSubscribed checks if channel is subscribed
+func (conn *Connection) isSubscribed(channel *Channel) bool {
+	return conn.channels.Has(channel.Name())
 }
