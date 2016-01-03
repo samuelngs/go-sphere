13afc23c638f6f0478f1fc26a87d1d7101894f96
Samuel <sam@infinitely.io>
2016-01-03 15:25:15 -0700

    rename connection_map.go to map_connection.go

--- map_connection.go
+++ map_connection.go
@@ -6,43 +6,43 @@ import (
 	"sync"
 )
 
-// ConnectionShardCount nums of shard
-var ConnectionShardCount = 32
+// connectionShardCount nums of shard
+var connectionShardCount = 32
 
-// ConnectionMap is a "thread" safe map of type string:Anything.
-// To avoid lock bottlenecks this map is dived to several (ConnectionShardCount) map shards.
-type ConnectionMap []*ConnectionMapShared
+// connectionmap is a "thread" safe map of type string:Anything.
+// To avoid lock bottlenecks this map is dived to several (connectionShardCount) map shards.
+type connectionmap []*connectionmapshared
 
-// ConnectionMapShared is a "thread" safe string to anything map.
-type ConnectionMapShared struct {
+// connectionmapshared is a "thread" safe string to anything map.
+type connectionmapshared struct {
 	items        map[string]*Connection
 	sync.RWMutex // Read Write mutex, guards access to internal map.
 }
 
-// ConnectionTuple used by the Iter & IterBuffered functions to wrap two variables together over a channel,
-type ConnectionTuple struct {
+// connectiontuple used by the Iter & IterBuffered functions to wrap two variables together over a channel,
+type connectiontuple struct {
 	Key string
 	Val *Connection
 }
 
-// NewConnectionMap Creates a new concurrent map.
-func NewConnectionMap() ConnectionMap {
-	m := make(ConnectionMap, ConnectionShardCount)
-	for i := 0; i < ConnectionShardCount; i++ {
-		m[i] = &ConnectionMapShared{items: make(map[string]*Connection)}
+// newConnectionMap Creates a new concurrent map.
+func newConnectionMap() connectionmap {
+	m := make(connectionmap, connectionShardCount)
+	for i := 0; i < connectionShardCount; i++ {
+		m[i] = &connectionmapshared{items: make(map[string]*Connection)}
 	}
 	return m
 }
 
 // GetShard returns shard under given key
-func (m ConnectionMap) GetShard(key string) *ConnectionMapShared {
+func (m connectionmap) GetShard(key string) *connectionmapshared {
 	hasher := fnv.New32()
 	hasher.Write([]byte(key))
-	return m[uint(hasher.Sum32())%uint(ConnectionShardCount)]
+	return m[uint(hasher.Sum32())%uint(connectionShardCount)]
 }
 
 // Set sets the given value under the specified key.
-func (m *ConnectionMap) Set(key string, value *Connection) {
+func (m *connectionmap) Set(key string, value *Connection) {
 	// Get map shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -51,7 +51,7 @@ func (m *ConnectionMap) Set(key string, value *Connection) {
 }
 
 // SetIfAbsent sets the given value under the specified key if no value was associated with it.
-func (m *ConnectionMap) SetIfAbsent(key string, value *Connection) bool {
+func (m *connectionmap) SetIfAbsent(key string, value *Connection) bool {
 	// Get map shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -64,7 +64,7 @@ func (m *ConnectionMap) SetIfAbsent(key string, value *Connection) bool {
 }
 
 // Get retrieves an element from map under given key.
-func (m ConnectionMap) Get(key string) (*Connection, bool) {
+func (m connectionmap) Get(key string) (*Connection, bool) {
 	// Get shard
 	shard := m.GetShard(key)
 	shard.RLock()
@@ -76,9 +76,9 @@ func (m ConnectionMap) Get(key string) (*Connection, bool) {
 }
 
 // Count returns the number of elements within the map.
-func (m ConnectionMap) Count() int {
+func (m connectionmap) Count() int {
 	count := 0
-	for i := 0; i < ConnectionShardCount; i++ {
+	for i := 0; i < connectionShardCount; i++ {
 		shard := m[i]
 		shard.RLock()
 		count += len(shard.items)
@@ -88,7 +88,7 @@ func (m ConnectionMap) Count() int {
 }
 
 // Has looks up an item under specified key
-func (m *ConnectionMap) Has(key string) bool {
+func (m *connectionmap) Has(key string) bool {
 	// Get shard
 	shard := m.GetShard(key)
 	shard.RLock()
@@ -100,7 +100,7 @@ func (m *ConnectionMap) Has(key string) bool {
 }
 
 // Remove removes an element from the map.
-func (m *ConnectionMap) Remove(key string) {
+func (m *connectionmap) Remove(key string) {
 	// Try to get shard.
 	shard := m.GetShard(key)
 	shard.Lock()
@@ -109,20 +109,20 @@ func (m *ConnectionMap) Remove(key string) {
 }
 
 // IsEmpty checks if map is empty.
-func (m *ConnectionMap) IsEmpty() bool {
+func (m *connectionmap) IsEmpty() bool {
 	return m.Count() == 0
 }
 
 // Iter returns an iterator which could be used in a for range loop.
-func (m ConnectionMap) Iter() <-chan ConnectionTuple {
-	ch := make(chan ConnectionTuple)
+func (m connectionmap) Iter() <-chan connectiontuple {
+	ch := make(chan connectiontuple)
 	go func() {
 		// Foreach shard.
 		for _, shard := range m {
 			// Foreach key, value pair.
 			shard.RLock()
 			for key, val := range shard.items {
-				ch <- ConnectionTuple{key, val}
+				ch <- connectiontuple{key, val}
 			}
 			shard.RUnlock()
 		}
@@ -132,15 +132,15 @@ func (m ConnectionMap) Iter() <-chan ConnectionTuple {
 }
 
 // IterBuffered returns a buffered iterator which could be used in a for range loop.
-func (m ConnectionMap) IterBuffered() <-chan ConnectionTuple {
-	ch := make(chan ConnectionTuple, m.Count())
+func (m connectionmap) IterBuffered() <-chan connectiontuple {
+	ch := make(chan connectiontuple, m.Count())
 	go func() {
 		// Foreach shard.
 		for _, shard := range m {
 			// Foreach key, value pair.
 			shard.RLock()
 			for key, val := range shard.items {
-				ch <- ConnectionTuple{key, val}
+				ch <- connectiontuple{key, val}
 			}
 			shard.RUnlock()
 		}
@@ -149,8 +149,8 @@ func (m ConnectionMap) IterBuffered() <-chan ConnectionTuple {
 	return ch
 }
 
-// MarshalJSON reviles ConnectionMap "private" variables to json marshal.
-func (m ConnectionMap) MarshalJSON() ([]byte, error) {
+// MarshalJSON reviles connectionmap "private" variables to json marshal.
+func (m connectionmap) MarshalJSON() ([]byte, error) {
 	// Create a temporary map, which will hold all item spread across shards.
 	tmp := make(map[string]*Connection)
 
